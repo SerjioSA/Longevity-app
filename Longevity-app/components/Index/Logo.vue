1222bb3f884b9e31a4870f1e018bbe8baeb3f6ca
SerjioSA <37222543+SerjioSA@users.noreply.github.com>
2023-08-11 14:03:04 +0400

    - added some adaptivity and pictures

--- Longevity-app/components/Index/Logo.vue
+++ Longevity-app/components/Index/Logo.vue
@@ -2,8 +2,8 @@
 
 </script>
 <template>
-    <div class="w-full text-center bg-indigo-500 p-40">
-        <div class="xl:text-[25vh] lg:text-[15vh] md:text-[14vh] ">REVITALIZE</div>
-        <div class="xl:text-[2vh] lg:text-[1vh] md:text-[1vh] tracking-[5.5em]">YOUR HEALTH</div>
-    </div>
+   <div class="w-full bg-indigo-500 p-40">
+    <h1 class="text-center lg:text-[25vh] md:text-[15vh] sm:text-[5vh] p-10">REVITALIZE</h1>
+    <h1 class="text-center xl:text-[2vh] lg:text-[1.5vh] md:text-[1vh] sm:text-[0.5vh] tracking-[5.5em] pl-20">YOUR HEALTH</h1>
+</div>
 </template>
